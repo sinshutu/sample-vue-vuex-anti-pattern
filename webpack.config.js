83913e0edd0861018bff5df49fc18fc58a7b2666
NaotoSuzuki <suzuki.z2o.naoto@gmail.com>
2019-03-02 15:37:46 +0900

    プロジェクトが起動するように

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+
+  entry: fs.readdirSync(path.join(__dirname, 'examples')).reduce((entries, dir) => {
+    const fullDir = path.join(__dirname, 'examples', dir)
+    const entry = path.join(fullDir, 'app.js')
+    if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
+      entries[dir] = ['webpack-hot-middleware/client', entry]
+    }
+
+    return entries
+  }, {}),
+
+  output: {
+    path: path.join(__dirname, '__build__'),
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    publicPath: '/__build__/'
+  },
+
+  module: {
+    rules: [
+      { test: /\.vue$/, use: ['vue-loader'] },
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] }
+    ]
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          name: 'shared',
+          filename: 'shared.js',
+          chunks: 'initial'
+        }
+      }
+    }
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+
+}
